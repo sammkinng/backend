5b027f62b0bb7b44a82d86e251c2e2c7381920fc
sammkinng <fm568pk@gmail.com>
2024-01-01 12:55:15 +0530

    cloud cdb onfig

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,6 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm"
 const config : TypeOrmModuleOptions={
     type: 'postgres',
     url:process.env.POSTGRES_URL,
-    database: 'redpositive',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
 };
