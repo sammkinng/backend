20427803bbe5ecc768d0a0d3faee95c6d8c28cd2
sammkinng <fm568pk@gmail.com>
2024-01-01 12:49:37 +0530

    postgres cloud config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,10 +2,7 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm"
 
 const config : TypeOrmModuleOptions={
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'sammkinng',
+    url:process.env.POSTGRES_URL,
     database: 'redpositive',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
