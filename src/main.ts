7303daa9c23559db63757adb4a9c93e4ee7eeabe
sammkinng <fm568pk@gmail.com>
2024-01-02 10:50:48 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    // Enable CORS
+    app.enableCors();
+
     const config = new DocumentBuilder()
         .setTitle('RedPositive Backend Assignment')
         .setDescription('API to get data and send Email')
@@ -15,6 +18,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3001);
+    await app.listen(3000);
 }
 bootstrap();
